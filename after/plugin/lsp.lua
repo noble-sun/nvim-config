7283ee8bf98a94e574b7338122721c5f66ac5d99
noble-sun <matsubayashi.helio+github@gmail.com>
2025-07-16 14:28:41 -0300

    chore: update plugins

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,19 +6,16 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
-require('lspconfig').solargraph.setup({})
+-- require('lspconfig').solargraph.setup({})
 -- require('lspconfig').hls.setup({})
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'hls', 'lua_ls'},
+  -- haskell lsp --> hls
+  ensure_installed = {'lua_ls'},
   handlers = {
     lsp_zero.default_setup,
-    lua_ls = function()
-        local lua_opts = lsp_zero.nvim_lua_ls()
-        require('lspconfig').lua_ls.setup(lua_opts)
-    end,
   },
 })
